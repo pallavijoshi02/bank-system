ba74bc89bf2d2a129370614596977b347ad57269
pallavijoshi02 <pallavi@gkmit.co>
2019-08-21 14:25:36 +0530

    UI complete

---
+++ src/api/index.js
@@ -0,0 +1,71 @@
+import axios from 'axios'
+import currentUser from '../helper/auth';
+
+axios.defaults.withCredentials = true;
+
+class API {
+    constructor() {
+        this._app = null
+    }
+
+    get call() {
+        if (!this._lapp) {
+            this._lapp = axios.create({
+            })
+        }
+        return this._lapp
+    }
+    get app() {
+        if (!this._app) {
+            this._app = axios.create({
+                headers: {
+                    'Authorization': currentUser.access_token
+                }
+            })
+        }
+        return this._app
+    }
+
+    logout = () => {
+        currentUser.logout();
+        // window.location.replace(window.location.origin);
+    }
+
+    async changeLocale(locale) {
+        return this.app
+            .get(`/update_language/${locale}`)
+            .then((resp) => {
+                window.location.reload();
+            }).catch((e) => {
+                console.error(e)
+            })
+    }
+
+    parseError = (e) => {
+        try {
+            if (!!e && !!e.response && !!e.response.data) {
+                if (!!e.response.data.error) {
+                    if (e.response.data.error === 'Invalid token' || e.response.data.error === 'Token expired') {
+                        this.logout()
+                    }
+                    return e.response.data.error
+                }
+                if (!!e.response.data.errors) {
+                    if (!!e.response.data.errors.errors) {
+                        return e.response.data.errors.errors
+                    }
+                    return e.response.data.errors[0]
+                }
+
+
+            }
+        } catch (err) {
+            console.error(err)
+        }
+        return 'common error'
+    }
+}
+
+const instance = new API()
+
+export default instance;
--- src/views/userAccounts.js
+++ src/views/userAccounts.js
@@ -1,36 +1,47 @@
 import React, { Component } from 'react';
-import MUIDataTable from "mui-datatables";    
+import MUIDataTable from "mui-datatables";
 import Sidebar from '../common/drawer';
 import Header from '../common/header';
+import { connect } from 'react-redux';
+//import api from '../api/index'
+import axios from 'axios';
+
 
 class UserAccounts extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            open: false,
-            name: '',
-            email: '',
-            phoneNo: '',
-            date: '',
-            occupation: ''
+            userData: '',
         }
     }
-
+    
     componentDidMount() {
-        console.log("data")
+        axios.get(`http://dhruvil-bank-app.herokuapp.com/users`)
+            .then(resp => {
+                //on success
+                console.log("data", resp.data)
+                this.setState({
+                    userData: resp.data
+                });
+            }).catch((error) => {
+                //on error
+                alert("There is an error in API call.");
+            });
     }
 
     handleClick() {
+        axios.get(`http://localhost:3000/users`)
+        .then(data => {
+            //on success
+            console.log("data", data)
+            this.setState({
+                userData: data.data,
+            });
+        }).catch((error) => {
+            //on error
+            alert("There is an error in API call.");
+        });
         window.location.replace('/transactions')
-    }
-    resetValues() {
-        this.setState({
-            name: '',
-            email: '',
-            phoneNo: '',
-            date: '',
-            occupation: ''
-        })
     }
 
     handleClose() {
@@ -39,6 +50,7 @@ class UserAccounts extends Component {
         })
         this.resetValues();
     }
+
     renderButton() {
         if (this.state.mode === 'view') {
             return (
@@ -50,7 +62,7 @@ class UserAccounts extends Component {
             return (
                 <button name="delete">
                     delete
-              </button>
+                </button>
             );
         }
     }
@@ -73,16 +85,9 @@ class UserAccounts extends Component {
         var button = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleClick.bind(this)}>
             <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
         </svg>
-        const columns = ["Account No.", "Account Type", "View"];
 
-        const userAccountsData = [
-            
-            ["1", "20-5-2019", button],
-            ["2", "21-5-2019", button],
-            ["3", "22-5-2019", button],
-            ["4", "23-5-2019", button],
-            ["5", "24-5-2019", button],
-        ];
+        const columns = ["Account No.", "Account Type", "View"];
+        const userAccountsData = [ this.state.userData ,button ];
 
         const options = {
             filterType: 'checkbox',
@@ -100,7 +105,7 @@ class UserAccounts extends Component {
                                 <h2 className="card-title">User Accounts</h2>
                                 {/* <button type="submit" className=" btn-default pull-right" onClick={this.handleClick.bind(this)}>Add User</button> */}
                             </div>
-                             <div className="card-body">
+                            <div className="card-body">
                                 <MUIDataTable
                                     title={"User Table"}
                                     data={userAccountsData}
@@ -115,4 +120,15 @@ class UserAccounts extends Component {
     }
 }
 
-export default UserAccounts;
+
+
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(UserAccounts);
+export { connectedApp as UserAccounts };
+
